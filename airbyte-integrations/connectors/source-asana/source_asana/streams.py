8ef0d9c3b24eecc9b0e4b33696496f98301b6553
sajarin <sajarin@users.noreply.github.com>
2023-10-09 07:40:32 +0000

    Automated Commit - Formatting Changes

--- airbyte-integrations/connectors/source-asana/source_asana/streams.py
+++ airbyte-integrations/connectors/source-asana/source_asana/streams.py
@@ -172,9 +172,11 @@ class Tags(WorkspaceRequestParamsRelatedStream):
     def path(self, **kwargs) -> str:
         return "tags"
 
+
 class PortfolioMemberships(AsanaStream):
     def path(self, **kwargs) -> str:
         return "portfolio_memberships"
+
 
 class Tasks(ProjectRelatedStream):
     def path(self, **kwargs) -> str:
